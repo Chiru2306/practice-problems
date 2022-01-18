2dab813b2da903598f3cc1b70ac0b52d3250e624
Chiru2306 <91423744+Chiru2306@users.noreply.github.com>
2022-01-18 18:47:46 +0530

    Create factorial.java

---
+++ factorial/factorial.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main{
+  pubic static void main(string[]args)
+    {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt;
+    int fact = 1;
+    for(int i=a;i>0;i--)
+    fact=fact*i;
+    System.out.println("fact");
+    }
+}
